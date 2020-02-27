71e5c72e9508ace9b5342b93f14d539a241f3632
Supasara Prasertphong <stpraser18@g.ucla.edu>
2020-02-27 07:42:57 +0000

    added US map into code

--- hw3/census_data/helpers.R
+++ hw3/census_data/helpers.R
@@ -1,54 +1,68 @@
-# Note: percent map is designed to work with the counties data set
-# It may not work correctly with other data sets if their row order does 
-# not exactly match the order in which the maps package plots counties
+library(sf)
+library(ggplot2)
+library(wesanderson)
 percent_map <- function(var, color, legend.title, min = 0, max = 100) {
   
-  # generate vector of fill colors for map
-  shades <- colorRampPalette(c("white", color))(100)
+(confirmed <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"))
+(recovered <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"))
+(death <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"))
   
-  # constrain gradient to percents that occur between min and max
-  var <- pmax(var, min)
-  var <- pmin(var, max)
-  percents <- as.integer(cut(var, 100, 
-                             include.lowest = TRUE, ordered = TRUE))
-  fills <- shades[percents]
+  us_map <- ggplot(data = us_states,
+                   mapping = aes(x = long, y = lat,
+                                 group = group))
+  us_map + geom_polygon(fill = "white", color = "black")
   
-  library(sf)
-  chn_map <- st_read("./bou2_4p.shp", as_tibble = TRUE) %>%
-    mutate(NAME = iconv(NAME, from = "GBK"),
-           BOU2_4M_ = as.integer(BOU2_4M_),
-           BOU2_4M_ID = as.integer(BOU2_4M_ID)) %>%
-    mutate(NAME = str_replace_na(NAME, replacement = "澳门特别行政区")) %>%
-    print()
+  #chn_map %>%
+    #ggplot() + 
+    #geom_sf(mapping = aes(geometry = geometry), color = "black", fill = "white") + 
+   ##geom_sf_label(mapping = aes(label = NAME)) + 
+    #theme_bw() # better for maps 
   
-  library(ggplot2)
-  chn_map %>%
-    ggplot() + 
-    geom_sf(mapping = aes(geometry = geometry), color = "black", fill = "white") + 
-    #geom_sf_label(mapping = aes(label = NAME)) + 
-    theme_bw() # better for maps 
+  confirmed_long <- confirmed %>%
+    pivot_longer(-(`Province/State`:Long), 
+                 names_to = "Date", 
+                 values_to = "confirmed") %>%
+    mutate(Date = (ymd(Date))) 
+  confirmed_long
   
+  recovered_long <- recovered %>%
+    pivot_longer(-(`Province/State`:Long), 
+                 names_to = "Date", 
+                 values_to = "recovered") %>%
+    mutate(Date = mdy(Date))
+  recovered_long
+  
+  
+  death_long <- death %>%
+    pivot_longer(-(`Province/State`:Long), 
+                 names_to = "Date", 
+                 values_to = "death") %>%
+    mutate(Date = mdy(Date))
+  death_long
+  
+
+
   # plot choropleth map
-  map("county", fill = TRUE, col = fills, 
-      resolution = 0, lty = 0, projection = "polyconic", 
-      myborder = 0, mar = c(0,0,0,0))
+  #map("county", fill = TRUE, col = fills, 
+      #resolution = 0, lty = 0, projection = "polyconic", 
+      #myborder = 0, mar = c(0,0,0,0))
   
   # overlay state borders
-  map("state", col = "white", fill = FALSE, add = TRUE,
-      lty = 1, lwd = 1, projection = "polyconic", 
-      myborder = 0, mar = c(0,0,0,0))
+ # map("state", col = "white", fill = FALSE, add = TRUE,
+     # lty = 1, lwd = 1, projection = "polyconic", 
+     # myborder = 0, mar = c(0,0,0,0))
   
   # add a legend
-  inc <- (max - min) / 4
-  legend.text <- c(paste0(min, " % or less"),
-                   paste0(min + inc, " %"),
-                   paste0(min + 2 * inc, " %"),
-                   paste0(min + 3 * inc, " %"),
-                   paste0(max, " % or more"))
+  #inc <- (max - min) / 4
+  #legend.text <- c(paste0(min, " % or less"),
+                  # paste0(min + inc, " %"),
+                  # paste0(min + 2 * inc, " %"),
+                  # paste0(min + 3 * inc, " %"),
+                   #paste0(max, " % or more"))
   
-  legend("bottomleft", 
-         legend = legend.text, 
-         fill = shades[c(1, 25, 50, 75, 100)], 
-         title = legend.title)
+  #legend("bottomleft", 
+        # legend = legend.text, 
+         #fill = shades[c(1, 25, 50, 75, 100)], 
+         #title = legend.title)
 }
 
